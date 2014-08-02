c89a397755e174e0117f678daceace07fe9dc2fa
gernest <geofreyernest@live.com>
2014-08-02 19:15:50 +0300

    Making sure Sinatra is loadded

--- lib/blogdown.rb
+++ lib/blogdown.rb
@@ -1,8 +1,12 @@
 require 'pathname'
+require 'sinatra'
+require 'thor'
+
 require "blogdown/version"
 require 'blogdown/exceptions'
 require 'blogdown/file_pipeline'
 require 'blogdown/publisher'
+require 'blogdown/app/app'
 require 'blogdown/cli'
 
 module Blogdown
