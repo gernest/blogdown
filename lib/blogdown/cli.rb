39c886e2d44bc32455e616c8d969ae7dce5eaf9c
gernest <geofreyernest@live.com>
2014-08-03 20:18:09 +0300

    Added some comments

--- lib/blogdown/cli.rb
+++ lib/blogdown/cli.rb
@@ -1,10 +1,12 @@
-
 module Blogdown
   module Cli
+    # Commandline app for blogdown
     class CommandLine<Thor
 
       desc "build", "builds the html files"
       def build
+        # Builds html files from markdown files, located in output
+        # and posts folders respectively
         begin
           publisher=Blogdown::Publisher.new(Dir.pwd)
           publisher.compose
@@ -15,10 +17,9 @@ module Blogdown
 
       desc "server", "serves the project for preview"
       def server
+        # Runs Sinatra built in server on the project root drectory
         ENV['BD']=Dir.pwd
-
         build
-
         Blogdown::PreviewApp.run!
       end
     end
