6012f6d64096ead5e1d48df0b3cd7f7931affd6b
gernest <geofreyernest@live.com>
2014-08-07 01:24:59 +0300

    refactoring

--- features/step_definitions/blogdown_steps.rb
+++ features/step_definitions/blogdown_steps.rb
@@ -1,11 +1,11 @@
 When(/^I run command "(.*?)"$/) do |cmd|
-  args=Array.new
-  args<<cmd
-  Blogdown::Cli::CommandLine.start args
+  pending # express the regexp above with the code you wish you had
+
 end
 
 
 When(/^I visit "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
 end
 
 Then(/^I should see an Index Page$/) do
